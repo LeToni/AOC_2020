9e9dec05e88628f8d3ca9e334ac4912677b0daca
Toni Le <toni.le@outlook.de>
2020-12-05 11:02:02 +0100

    Day 5 Puzzle 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -12,17 +12,53 @@ namespace Day5
             var path = "Input.txt";
             var boardingCodes = ProcessInput(path);
 
+            Dictionary<int, bool> SeatIds = new Dictionary<int, bool>();
+            for(int i = 0; i <= 127; i++)
+            {
+                for(int j= 0; j <= 7; j++)
+                {
+                    var SeatId = i * 8 + j;
+                    SeatIds.Add(SeatId, true);
+                }
+            }
+
             var max = int.MinValue;
-            foreach(var boardingCode  in boardingCodes)
+            foreach (var boardingCode in boardingCodes)
             {
                 var row = CalculateRow(boardingCode.Substring(0, 7));
                 var col = CalculateCol(boardingCode.Substring(7, 3));
 
                 var seatID = row * 8 + col;
+                SeatIds.Remove(seatID);
                 max = Math.Max(max, seatID);
             }
 
             Console.WriteLine($"Highest SeatID found: {max}");
+
+            foreach(var seatId in SeatIds.Keys.ToList())
+            {
+                bool eval1 = false;
+                bool eval2 = false;
+                if(SeatIds.TryGetValue(seatId-1, out eval1) && SeatIds.TryGetValue(seatId + 1, out eval2))
+                {
+                    SeatIds[seatId - 1] = false;
+                    SeatIds[seatId] = false;
+                    SeatIds[seatId + 1] = false;
+                }
+            }
+
+            foreach (var seatId in SeatIds.Keys.ToList())
+            {
+                if(SeatIds[seatId] == false)
+                {
+                    SeatIds.Remove(seatId);
+                }
+            }
+
+            foreach (var seatId in SeatIds.Keys.ToList())
+            {
+                Console.WriteLine($"Your seat should be: {seatId}");
+            }
         }
 
         private static List<string> ProcessInput(string file)
