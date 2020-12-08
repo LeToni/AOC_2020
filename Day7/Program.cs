45d562ab8e7bbad468b00bd510a6857013790518
Toni Le <toni.le@outlook.de>
2020-12-08 20:45:32 +0100

    Day07

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -9,21 +9,77 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            
-            string path = "Input_Test.txt";
+            string path = "Input.txt";
             Dictionary<string, Bag> bags = new Dictionary<string, Bag>();
             ProcessInput(path, ref bags);
 
             var count = SearchForAmountOfBagWithColor(bags, "shiny gold");
-            Console.WriteLine(count);
+            Console.WriteLine($"Bags that can eventually contain one shiny gold: {count}");
+
+            var amount = CountBagContainsAmountOfBag("shiny gold", bags);
+            Console.WriteLine($"Amount of individual bags are required: {amount}");
+        }
+
+        public static int CountBagContainsAmountOfBag(string color, Dictionary<string,Bag> bags)
+        {
+            return CountAmountOfBagsContained(color, bags) - 1;
+        }
+
+        public static int CountAmountOfBagsContained(string currentBag, Dictionary<string, Bag> bags)
+        {
+            int count = 1;
+            if (!bags.ContainsKey(currentBag))
+            {
+                return count;
+            }
+
+            var bag = bags[currentBag];
+
+            foreach(var b in bag.Content)
+            {
+                count = count + b.Amount * CountAmountOfBagsContained(b.ColorBag, bags);
+            }
+
+            return count;
         }
 
         public static int SearchForAmountOfBagWithColor(Dictionary<string, Bag> bags, string color)
         {
+            int count = 0;
+            foreach(var bag in bags)
+            {
+                if (ContainsBagWithColor(bag.Key, color, bags))
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         public static bool ContainsBagWithColor(string start, string color, Dictionary<string, Bag> bags)
         {
+            if(!bags.ContainsKey(start))
+            {
+                return false;
+            }
+
+            var bag = bags[start];
+
+            foreach(var subBag in bag.Content)
+            {
+                if (subBag.ColorBag == color)
+                {
+                    return true;
+                }
+
+                if (ContainsBagWithColor(subBag.ColorBag, color, bags))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public static void ProcessInput(string file, ref Dictionary<string, Bag> bags)
