bda074b1734d30e2a680033cb6c7386d83c79eef
Toni Le <toni.le@outlook.de>
2020-12-08 08:46:02 +0100

    Day 07 Refactoring (In Progress)

---
+++ Day7/Bag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Day7
+{
+    public class Bag
+    {
+        public string Name { get; set; }
+        public List<(string ColorBag, int Amount)> Content { get; set; } = new List<(string ColorBag, int Amount)>();
+
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = "Input_Test.txt";
+            Dictionary<string, Bag> bags = new Dictionary<string, Bag>();
+            ProcessInput(path, ref bags);
+
+            var count = SearchForAmountOfBagWithColor(bags, "shiny gold");
+            Console.WriteLine(count);
+        }
+
+        public static int SearchForAmountOfBagWithColor(Dictionary<string, Bag> bags, string color)
+        {
+        }
+
+        public static bool ContainsBagWithColor(string start, string color, Dictionary<string, Bag> bags)
+        {
+        }
+
+        public static void ProcessInput(string file, ref Dictionary<string, Bag> bags)
+        {
+            var rules = File.ReadAllLines(file);
+
+            Regex nameBagFilter = new Regex("(.*?) bags contain");
+            Regex ruleFilter = new Regex("(\\d+|no) (.*?) bag");
+
+            foreach (var rule in rules)
+            {
+                var matchBagName = nameBagFilter.Match(rule);
+                string bagName;
+                if (matchBagName.Success)
+                {
+                    bagName = matchBagName.Groups[1].Value;
+                    var matchBagRules = ruleFilter.Matches(rule);
+
+                    var bag = new Bag();
+                    foreach (Match match in matchBagRules)
+                    {
+                        if(match.Groups[2].Value == "other"){
+                            bag.Name = bagName;
+                            bag.Content.Add(("other", 0));
+                        }
+                        else
+                        {
+                            bag.Name = bagName;
+                            bag.Content.Add((match.Groups[2].Value, int.Parse(match.Groups[1].Value)));
+                        }
+                    }
+                    bags.Add(bagName, bag);
+                }
+
+             }
+        }
+
+    }
+}
